3dd2127fe3ba9115397452bba94c5a2a23ac912a
Oleg Toporkov <oleg.toporkov@auto1.com>
2020-01-29 23:20:25 +0100

    Added hamcrest to assert values and replaced content type to constant

--- src/test/java/com/up42/api/e2e/LoginTest.java
+++ src/test/java/com/up42/api/e2e/LoginTest.java
@@ -3,11 +3,15 @@ package com.up42.api.e2e;
 import com.up42.api.constants.Endpoints;
 import com.up42.api.dtos.login.LoginResponseDto;
 import com.up42.api.properties.TestProperties;
+import io.restassured.http.ContentType;
 import org.junit.jupiter.api.Test;
 
 import static com.up42.api.clients.base.BaseClient.BASE_REQUEST_SPEC;
 import static com.up42.api.clients.base.BaseClient.BASE_RESPONSE_SPEC;
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.hamcrest.Matchers.nullValue;
 
 public class LoginTest {
 
@@ -20,7 +24,7 @@ public class LoginTest {
                     .auth()
                     .basic(TestProperties.CONFIG.getProject().getId(),
                            TestProperties.CONFIG.getProject().getKey())
-                    .contentType("application/x-www-form-urlencoded")
+                    .contentType(ContentType.URLENC)
                     .body("grant_type=client_credentials").
             when()
                     .post(Endpoints.OAUTH_TOKEN).
@@ -30,6 +34,8 @@ public class LoginTest {
                     .body()
                     .as(LoginResponseDto.class);
         //@formatter:on
+        assertThat(responseDto.getAccessToken(), notNullValue());
+        assertThat(responseDto.getError(), nullValue());
     }
 
 }
