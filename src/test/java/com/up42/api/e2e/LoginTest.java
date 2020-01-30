c8890c0487203ed43b9439cb904a91071be15010
Oleg Toporkov <oleg.toporkov@auto1.com>
2020-01-30 21:15:13 +0100

    Added negative login and workflow tests

--- src/test/java/com/up42/api/e2e/LoginTest.java
+++ src/test/java/com/up42/api/e2e/LoginTest.java
@@ -1,14 +1,9 @@
 package com.up42.api.e2e;
 
-import com.up42.api.constants.Endpoints;
+import com.up42.api.clients.auth.OAuthClient;
 import com.up42.api.dtos.login.LoginResponseDto;
-import com.up42.api.properties.TestProperties;
-import io.restassured.http.ContentType;
 import org.junit.jupiter.api.Test;
 
-import static com.up42.api.clients.base.BaseClient.BASE_REQUEST_SPEC;
-import static com.up42.api.clients.base.BaseClient.BASE_RESPONSE_SPEC;
-import static io.restassured.RestAssured.given;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.notNullValue;
 import static org.hamcrest.Matchers.nullValue;
@@ -17,23 +12,8 @@ public class LoginTest {
 
     @Test
     void successfulLogin() {
-        //@formatter:off
-        LoginResponseDto responseDto =
-            given()
-                    .spec(BASE_REQUEST_SPEC)
-                    .auth()
-                    .basic(TestProperties.CONFIG.getProject().getId(),
-                           TestProperties.CONFIG.getProject().getKey())
-                    .contentType(ContentType.URLENC)
-                    .body("grant_type=client_credentials").
-            when()
-                    .post(Endpoints.OAUTH_TOKEN).
-            then()
-                    .spec(BASE_RESPONSE_SPEC)
-                    .extract()
-                    .body()
-                    .as(LoginResponseDto.class);
-        //@formatter:on
+        LoginResponseDto responseDto = OAuthClient.loginAsDefaultUser();
+
         assertThat(responseDto.getAccessToken(), notNullValue());
         assertThat(responseDto.getError(), nullValue());
     }
